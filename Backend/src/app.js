631d96327cc72884a65498c913af0f92b01204be
Shradha Choudhary <shradha.choudhary04@gmail.com>
2026-08-09 11:14:50 +0530

    Update app.js
    MongoDB Connection change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -19,7 +19,7 @@ app.use("/api/v1/users",userRoutes)
 
 const start=async()=>{
     const connectionDB = await mongoose.connect(
-      "mongodb+srv://chyshradha_db_user:chy.shr@cluster0.b6klqgs.mongodb.net/",
+        "mongodb+srv://shradhachoudhary04_db_user:2pRUs4LiC6KgebK3@cluster0.igwvovg.mongodb.net/?appName=Cluster0",
     );
     console.log(`MONGO Connectd DB Host: ${connectionDB.connection.host}`)
     server.listen(app.get("port"),()=>{
